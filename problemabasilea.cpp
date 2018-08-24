237b2ff9f9415ca9354880d4d70c0a30b0a9f3ab
HardCode <warl0ckofshadow@gmail.com>
2018-08-24 20:36:38 +0200

    Version 0.2

--- problemabasilea.cpp
+++ problemabasilea.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 #include <iomanip>
 #include <math.h>
+#include <ctype.h>
 
 using namespace std;
 
-int n = 0;
-double pi;
+void piloop(int b)
+{
+        double pi;
+        int n = 0;
+        for(int i = b; i > 0; --i)
+        {
+                pi = pi + (6/(pow(++n, 2)));
+        }
+        pi = sqrt(pi);
+        cout << "Pi: " << setprecision(10) << pi << endl;
+}
 
 int main(int argc, char* argv[])
 {
         if(argc > 1)
         {
-          for(int i = atoi(argv[1])-1; i >= 0; --i)
-          {
-                  pi = pi + (6/(pow(++n, 2)));
-          }
-          pi = sqrt(pi);
-          cout << "pi: " << setprecision(20) << pi << endl;
+                piloop(atoi(argv[1]));
         }
         else
         {
-          cout << "Uso: " << argv[0] << " numero_iteracciones" << endl;
+                int iteracciones;
+                cout << "Tambien puedes usar: " << argv[0] << " numero_iteracciones" << endl << "Numero iteracciones: ";
+                cin >> iteracciones;
+                piloop(iteracciones);
+
         }
 
         return 0;
